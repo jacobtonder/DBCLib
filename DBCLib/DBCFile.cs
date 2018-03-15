1bc8d5ceffc49356c2af6759787cdf928c255c15
JacobTonder <hunnichehunniche@gmail.com>
2018-03-15 15:45:38 +0100

    DBCLib/DBCFile: Added initial start on LoadData method in DBCFile

--- DBCLib/DBCFile.cs
+++ DBCLib/DBCFile.cs
@@ -3,10 +3,22 @@
     public class DBCFile
     {
         public string FilePath { get; }
+        public bool IsLoaded { get; private set; }
 
         public DBCFile(string path)
         {
             FilePath = path;
+            IsLoaded = false;
+        }
+
+        public void LoadData()
+        {
+            if (IsLoaded)
+                return;
+
+            IsLoaded = true;
+
+            return;
         }
     }
 }
