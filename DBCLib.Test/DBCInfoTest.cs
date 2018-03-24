9115c33cf9a968b3f04e4eb504fba7f482a1e365
JacobTonder <hunnichehunniche@gmail.com>
2018-03-24 22:38:39 +0100

    DBCLib.Test/DBCInfoTest: Added some tests to DBCInfo

---
+++ DBCLib.Test/DBCInfoTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DBCLib.Test
+{
+    [TestClass]
+    public class DBCInfoTest
+    {
+        private DBCInfo dbcInfo;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            dbcInfo = new DBCInfo(0, 1, 2, 3);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            dbcInfo = default(DBCInfo);
+        }
+
+        [TestMethod]
+        public void DBCRecords_AreEqual()
+        {
+            Assert.AreEqual((uint)0, dbcInfo.DBCRecords);
+        }
+
+        [TestMethod]
+        public void DBCFields_AreEqual()
+        {
+            Assert.AreEqual((uint)1, dbcInfo.DBCFields);
+        }
+
+        [TestMethod]
+        public void RecordSize_AreEqual()
+        {
+            Assert.AreEqual((uint)2, dbcInfo.RecordSize);
+        }
+
+        [TestMethod]
+        public void StringSize_AreEqual()
+        {
+            Assert.AreEqual((uint)3, dbcInfo.StringSize);
+        }
+    }
+}
