ebf5fe0fbdd6469d28cab5374ee42a645042fa6d
JacobTonder <hunnichehunniche@gmail.com>
2020-01-05 13:17:39 +0100

    DBCLib/Types: Moved constructor to the top of LocalizedString

--- DBCLib/Types/LocalizedString.cs
+++ DBCLib/Types/LocalizedString.cs
@@ -2,11 +2,12 @@
 {
     public class LocalizedString
     {
+        public LocalizedString(string str) => String = str;
+
         // Size of Localized Strings
         public static readonly int Size = 17;
         public string String { get; set; }
 
-        public LocalizedString(string str) => String = str;
         public static implicit operator LocalizedString(string str) => new LocalizedString(str);
         public static implicit operator string(LocalizedString str) => str.String;
     }
